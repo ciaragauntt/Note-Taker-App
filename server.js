326585685c7928a63427969a3d6d70ed3c392937
cici <ci.gauntt@gmail.com>
2022-07-08 13:52:57 -0600

    added the api route, running into some issues with the deployed application, will ask in class tomorrow

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-const noteData = require('./db/db.json')
+const noteData = require('./Develop/db/db.json')
 const router = require('express').Router();
 
 //setting up data parsing 
@@ -22,7 +22,40 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
     );
 
+// api route
+
+router.get('/notes', (req, res) => {
+    store 
+    .getNotes()
+    .then(notes => {
+        res.json(notes)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    });
+});
+
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+    noteData
+    .addNote(req.body)
+    .then(note => {
+        res.json(note)
+    })
+    .cath(err => {
+        res.status(500).json(err)
+    });
+});
+
+router.delete('/notes/id:', (req, res) => {
+    noteData
+    .removeNote(req.params.id)
+    .then(() => res.json({okay: true}))
+    .catch(err => res.status(500).json(err))
+});
+
+module.exports = router;
+
 app.listen(PORT, () =>
     console.log(`Application listening at http://localhost:${PORT}`)
     );
-
